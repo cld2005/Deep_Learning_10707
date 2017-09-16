90a749b1c189505b208d4675a65594cff088bc56
cld2005 <cld2005@live.cn>
2017-09-16 15:27:03 -0400

    creating functions

---
+++ DLHW1/d_sigmoid.m
@@ -0,0 +1,2 @@
+function y = d_sigmoid(x)
+y=sigmoid(x)*(1.0-sigmoid(x));
--- DLHW1/dataLoad.m
+++ DLHW1/dataLoad.m
@@ -1,10 +1,20 @@
 function [x_train,y_train,x_validate,y_validate,x_test,y_test] = dataLoad()
-tr_data=textread('digitstrain.txt','','delimiter',',');
-x_train=tr_data(:,1:784);
-y_train=tr_data(:,785);
-va_data=textread('digitsvalid.txt','','delimiter',',');
-x_validate=va_data(:,1:784);
-y_validate=va_data(:,785);
-ts_data=textread('digitstest.txt','','delimiter',',');
-x_test=ts_data(:,1:784);
-y_test=ts_data(:,785);
+fprintf('exist x_train %d ...\n',exist('x_train'))
+if(exist('x_train')==0)
+    fprintf('reading xtrain ...\n')
+    tr_data=textread('digitstrain.txt','','delimiter',',');
+    x_train=tr_data(:,1:784);
+    y_train=tr_data(:,785);
+end
+if(exist('x_validate')==0)
+    va_data=textread('digitsvalid.txt','','delimiter',',');
+    x_validate=va_data(:,1:784);
+    y_validate=va_data(:,785);   
+end
+
+if(exist('x_test')==0)
+    ts_data=textread('digitstest.txt','','delimiter',',');
+    x_test=ts_data(:,1:784);
+    y_test=ts_data(:,785);
+end
+end
