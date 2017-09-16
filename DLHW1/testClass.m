0b7aad0cff07a7ee8f972d9cd66d34c68231c120
Lindi Chen <lindic@bh140d-a1.andrew.cmu.edu>
2017-09-16 15:47:33 -0400

    push

---
+++ DLHW1/testClass.m
@@ -0,0 +1,16 @@
+classdef testClass
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        list= [1 2 3 4 5];
+    end
+    
+    methods
+        function y=maxEle(obj)
+            y=max(obj.list);
+        end
+    end
+    
+end
+
