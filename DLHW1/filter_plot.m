f934197f53afddc272f63cc0e614f9754e3265a0
Lindi Chen <lindic@bh140d-a3.andrew.cmu.edu>
2017-09-19 16:58:00 -0400

    normalzie filter

--- DLHW1/filter_plot.m
+++ DLHW1/filter_plot.m
@@ -1,5 +1,8 @@
 function filter_plot(ann,filters)
-
+%mat2gray
 for i=0:filters-1
-    subplot(10,ceil(filters/10),i+1),imshow(mat2gray(reshape(ann.weights{2}(i+1,:),[28,28])))
+    minv=min(ann.weights{2}(i+1,:));
+    maxv=max(ann.weights{2}(i+1,:));
+    norm=uint8((ann.weights{2}(i+1,:)-minv)*255/(maxv-minv));
+    subplot(10,ceil(filters/10),i+1),imshow(uint8(255*mat2gray(reshape(norm,[28,28]))));
 end
--- DLHW1/main.m
+++ DLHW1/main.m
@@ -3,7 +3,7 @@ num_hidden_neuron=100;
 learning_rate=0.1;
 batch_size=20;
 epoches=200;
-momentum=0.9;
+momentum=0;
 ann = ANN();
 ann.ANN_load_data();
 [train_error,vali_error]=ann.train(num_hidden_layer,num_hidden_neuron,learning_rate,batch_size,epoches,momentum);
