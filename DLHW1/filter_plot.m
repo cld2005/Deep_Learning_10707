39f7f99ec342c94952fd5a1077beb436e1d1f23f
Lindi Chen <lindic@bh140d-a3.andrew.cmu.edu>
2017-09-19 17:05:29 -0400

    normalzie filter

--- DLHW1/filter_plot.m
+++ DLHW1/filter_plot.m
@@ -4,5 +4,5 @@ for i=0:filters-1
     minv=min(ann.weights{2}(i+1,:));
     maxv=max(ann.weights{2}(i+1,:));
     norm=uint8((ann.weights{2}(i+1,:)-minv)*255/(maxv-minv));
-    subplot(10,ceil(filters/10),i+1),imshow(uint8(255*mat2gray(reshape(norm,[28,28]))));
+    subplot(10,ceil(filters/10),i+1),imshow(transpose(uint8(255*mat2gray(reshape(norm,[28,28])))));
 end
