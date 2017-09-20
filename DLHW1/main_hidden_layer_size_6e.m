d88adc5b7dfcef2e58bbe97c7850fd5e66a3076f
Lindi Chen <lindic@bh140d-b3.andrew.cmu.edu>
2017-09-20 19:49:22 -0400

    push

---
+++ DLHW1/main_hidden_layer_size_6e.m
@@ -0,0 +1,30 @@
+rounds=4;
+anns=cell(rounds,1);
+close all;
+num_hidden_size=[20,100,200,500];
+for i=1:rounds
+    fprintf('round %d\n',i );
+    %rng(i);
+    num_hidden_layer=1;
+    num_hidden_neuron=num_hidden_size(i);
+    learning_rate=0.01;
+    batch_size=1;
+    epoches=200;
+    momentum=0.5;
+    anns{i} = ANN();
+    anns{i}.ANN_load_data();
+    [train_error,vali_error]=anns{i}.train(num_hidden_layer,num_hidden_neuron,learning_rate,batch_size,epoches,momentum);
+    
+    %filter_plot(anns{i},num_hidden_neuron,i);
+end
+plot_stats_all(anns);
+%for i=1:rounds
+    %plot_stats(anns{i}.train_error,anns{i}.vali_error);
+%end
+for i=1:size(anns,1)
+    ann = anns{i};
+    save(['ann_hidden_layer' num2str(learning_rates(i)) '.mat'],'ann');
+end
+
+
+
---
+++ DLHW1/main_momentum.m
@@ -0,0 +1,30 @@
+rounds=3;
+anns=cell(rounds,1);
+close all;
+momentums=[0,0.5,0.9];
+for i=1:rounds
+    fprintf('round %d\n',i );
+    %rng(i);
+    num_hidden_layer=1;
+    num_hidden_neuron=100;
+    learning_rate=0.1;
+    batch_size=1;
+    epoches=200;
+    momentum=momentums(i);
+    anns{i} = ANN();
+    anns{i}.ANN_load_data();
+    [train_error,vali_error]=anns{i}.train(num_hidden_layer,num_hidden_neuron,learning_rate,batch_size,epoches,momentum);
+    
+    %filter_plot(anns{i},num_hidden_neuron,i);
+end
+plot_stats_all(anns);
+%for i=1:rounds
+    %plot_stats(anns{i}.train_error,anns{i}.vali_error);
+%end
+for i=1:size(anns,1)
+    ann = anns{i};
+    save(['ann_momentum' num2str(momentums(i)) '.mat'],'ann');
+end
+
+
+
