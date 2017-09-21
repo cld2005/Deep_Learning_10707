50367d8746df20e736564e5370a1330a993b256a
Lindi Chen <lindic@bh140d-b3.andrew.cmu.edu>
2017-09-21 09:47:43 -0400

    push

--- DLHW1/main_momentum.m
+++ DLHW1/main_momentum.m
@@ -9,23 +9,31 @@ for i=1:rounds
     num_hidden_neuron=100;
     learning_rate=0.1;
     batch_size=1;
-    epoches=200;
+    epoches=1;
     momentum=momentums(i);
     anns{i} = ANN();
     anns{i}.ANN_load_data();
     [train_error,vali_error]=anns{i}.train(num_hidden_layer,num_hidden_neuron,learning_rate,batch_size,epoches,momentum);
-    
+    ann = anns{i};
+    save(['ann_momentum' num2str(momentums(i)) '.mat'],'ann');
+    try
+        save(['ann_momentum' num2str(momentums(i)) '.mat'],'ann');
+    catch exception
+        fprintf('save round %d failed\n',i);
+    end
+
     %filter_plot(anns{i},num_hidden_neuron,i);
 end
 plot_stats_all(anns);
 %for i=1:rounds
     %plot_stats(anns{i}.train_error,anns{i}.vali_error);
 %end
-for i=1:size(anns,1)
-    ann = anns{i};
-    save(['ann_momentum' num2str(momentums(i)) '.mat'],'ann');
+try
+    save('anns_momentum.mat','anns');
+catch exception
+    fprintf('final save failed\n')
 end
-save('ann_momentum.mat','anns');
+
 
 
 
