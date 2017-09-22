54c34d293717a4d744e235f60a7ae489dd521d6d
cld2005 <cld2005@live.cn>
2017-09-21 23:50:21 -0400

    push before 2 layers

--- DLHW1/main_2_layer_learning_rate.m
+++ DLHW1/main_2_layer_learning_rate.m
@@ -9,7 +9,7 @@ for i=1:rounds
     num_hidden_neuron=100;
     learning_rate=learning_rates(i);
     batch_size=1;
-    epoches=1;
+    epoches=200;
     momentum=0;
     anns{i} = ANN();
     anns{i}.ANN_load_data();
