dae922c6ca3d2af0aadd68997fc198d112e23cb4
cld2005 <cld2005@live.cn>
2017-09-21 23:49:59 -0400

    push before 2 layers

--- DLHW1/main_2_layer_learning_rate.m
+++ DLHW1/main_2_layer_learning_rate.m
@@ -1,28 +1,35 @@
-rounds=3;
+rounds=4;
 anns=cell(rounds,1);
 close all;
-learning_rates=[0.01 0.2 0.5];
+learning_rates=[0.01 0.1 0.2 0.5];
 for i=1:rounds
     fprintf('round %d\n',i );
     %rng(i);
-    num_hidden_layer=1;
+    num_hidden_layer=2;
     num_hidden_neuron=100;
     learning_rate=learning_rates(i);
     batch_size=1;
-    epoches=200;
+    epoches=1;
     momentum=0;
     anns{i} = ANN();
     anns{i}.ANN_load_data();
+    anns{i}.set_lumbda(0);
     [train_error,vali_error]=anns{i}.train(num_hidden_layer,num_hidden_neuron,learning_rate,batch_size,epoches,momentum);
-    
+    ann = anns{i};
+    try
+        save(['ann_2_layer_learning_rate' num2str(learning_rates(i)) '.mat'],'ann');
+    catch exception
+        fprintf('save round %d failed\n',i);
+    end
     %filter_plot(anns{i},num_hidden_neuron,i);
 end
 plot_stats_all(anns);
-%for i=1:rounds
-    %plot_stats(anns{i}.train_error,anns{i}.vali_error);
-%end
-for i=1:size(anns,1)
-    ann = anns{i};
-    save(['ann_learning_rate' num2str(learning_rates(i)) '.mat'],'ann');
+
+try
+    save('anns_2_layer_learning_rate.mat','anns');
+catch exception
+    fprintf('final save failed\n');
 end
-save('anns_learning_rate.mat','anns');
+
+
+
