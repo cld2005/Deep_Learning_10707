4b21f5348b7a45cca09b81375fdfc5fe20816c31
Lindi Chen <lindic@bh140d-a1.andrew.cmu.edu>
2017-10-17 11:59:55 -0400

    push

--- DLHW2/LoadData.m
+++ DLHW2/LoadData.m
@@ -1,19 +1,19 @@
-function [ train,validate,test] = LoadData()
+function [ x_train,x_validate,x_test] = LoadData()
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
     fprintf('reading training data ...\n')
     tr_data=textread('../DLHW1/digitstrain.txt','','delimiter',',');
     tr_data = tr_data(randperm(size(tr_data,1)),:);%shuffle data
-    train=tr_data;
+     x_train=tr_data(:,1:784);
     
     
     fprintf('reading validation data ...\n')
     va_data=textread('../DLHW1/digitsvalid.txt','','delimiter',',');
-    validate=va_data;
+    x_validate=va_data(:,1:784);
 
     fprintf('reading test data ...\n')
     ts_data=textread('../DLHW1/digitstest.txt','','delimiter',',');
-    test=ts_data;
+    x_test=ts_data(:,1:784);
 
 end
 
