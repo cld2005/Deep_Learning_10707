da6c1e182b4ab291e996a6327fe6b12b5880fcc0
Lindi Chen <lindic@bh140d-a3.andrew.cmu.edu>
2017-10-17 17:26:01 -0400

    push

--- DLHW2/main.m
+++ DLHW2/main.m
@@ -8,9 +8,11 @@ for i=1:rounds
     num_hiddenn=100;
     learning_rate=0.1;
     batch_size=1;
-    epoches=5;
+    epoches=3;
     k=1;
     rbms{i} = RBM();
     [train_error,vali_error] = rbms{i}.train(num_visible,num_hiddenn,learning_rate,batch_size,epoches,k);
+    filter_plot(rbms{1},100,i);
 end
+
 plot_stats_all(rbms);
