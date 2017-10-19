9bc5925dfd96867f267731fbdec8d9bb76328809
Lindi Chen <lindic@bh140d-b2.andrew.cmu.edu>
2017-10-19 17:23:23 -0400

    before 5g rbm

--- DLHW2/plot_stats_all.m
+++ DLHW2/plot_stats_all.m
@@ -4,7 +4,11 @@ hold on
 title('Combined cross-entropy Plots')
 legendInfo={};
 line_width=2;
-for i=1:size(rbms,1)    
+for i=1:size(rbms,1)   
+    if isempty(rbms{i})
+        continue;
+    end 
+    
     plot(rbms{i}.train_error(:,1),'LineWidth',line_width)
     legendInfo{end+1}=['training cross-entropy' num2str(i)];
     plot(rbms{i}.vali_error(:,1),'LineWidth',line_width)
